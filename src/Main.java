59111ad375f1db8fb2b0a153c2e78e1bf6923a59
AnaTasevska1 <anabozinovska0@gmail.com>
2023-04-15 03:20:36 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("I am a student!");
+        System.out.println("I am a continuation to project");
     }
 }
